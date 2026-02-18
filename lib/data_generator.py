50f194ae7306aa9c85c92c16f8c49a3bb0e7f401
Jim Ochieng <jimochieng64@gmail.com>
2026-02-19 02:47:12 +0300

    Implement student filtering and set operations

--- lib/data_generator.py
+++ lib/data_generator.py
@@ -5,4 +5,5 @@ def student_generator(student_list, major):
     Generate student records filtered by major lazily for memory efficiency
     using a Python generator.
     """
-    pass
+    generator_expression_by_major = (student for student in student_list if major.lower() == student[2].lower())
+    return generator_expression_by_major
